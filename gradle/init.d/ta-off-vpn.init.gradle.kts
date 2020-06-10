644d8978d7a6b80df6bfb65de45f4f6829aecac6
Daniel Lin <ephemient@gmail.com>
2020-06-10 13:33:39 -0400

    More config

---
+++ gradle/init.d/ta-off-vpn.init.gradle.kts
@@ -0,0 +1,34 @@
+import java.io.File
+import java.net.InetAddress
+import java.net.URI
+import java.net.URISyntaxException
+import java.net.UnknownHostException
+
+val gitRemoteOriginUri: URI? by lazy {
+    startParameter.projectDir
+        ?.let { projectDir ->
+            ProcessBuilder("git", "remote", "get-url", "origin")
+                .directory(projectDir)
+                .redirectInput(File("/dev/null"))
+                .redirectOutput(ProcessBuilder.Redirect.PIPE)
+                .redirectError(File("/dev/null"))
+                .start()
+        }
+        ?.run { inputStream.use { it.bufferedReader().readText().trim() }.also { waitFor() }.takeIf { exitValue() == 0 } }
+        ?.let { try { URI(it).parseServerAuthority() } catch (_: URISyntaxException) { null } }
+}
+
+val isTripAdvisor: Boolean
+    get() {
+        val hostMatch = gitRemoteOriginUri?.host?.run { '.' + removeSuffix(".") }
+        return hostMatch?.endsWith(".github.com", ignoreCase = true) == true &&
+            gitRemoteOriginUri?.rawPath?.trimStart('/')?.startsWith("tamgio/") == true ||
+            hostMatch?.endsWith(".tripadvisor.com", ignoreCase = true) == true
+    }
+
+try {
+    if (isTripAdvisor) InetAddress.getByName("maven.dev.tripadvisor.com")
+} catch (_: UnknownHostException) {
+    logger.error("*** Using TripAdvisor project off VPN: setting gradle --offline ***")
+    startParameter.isOffline = true
+}
